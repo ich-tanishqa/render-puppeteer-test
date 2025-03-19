90b0b934c1b370f64da7908ed2d2827e87430ce0
ich-tanishqa <110523872+ich-tanishqa@users.noreply.github.com>
2025-03-19 10:41:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ import puppeteer from 'puppeteer';
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://www.google.co.in/');
+
+  // Set User-Agent to Chrome 80 (older version)
+  await page.setUserAgent(
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36"
+  );
+
+  await page.goto('https://www.zomato.com/');
 
   const title = await page.title();
   console.log(`Page title: ${title}`);
